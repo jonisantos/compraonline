a834b33b6d11f53a3dcec9d9c71e7422898a1566
JonilsonNascimento <jonnyzerobravo@gmail.com>
2019-05-27 16:28:47 -0400

    Redirect com Escopo de flash

--- src/main/java/br/com/compraonline/cotrollers/ProdutoController.java
+++ src/main/java/br/com/compraonline/cotrollers/ProdutoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.compraonline.daos.ProdutoDAO;
 import br.com.compraonline.models.Produto;
@@ -25,9 +26,10 @@ public class ProdutoController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String gravar(Produto produto){
+	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes){
 		dao.salvar(produto);
-		return "produtos/ok";
+		redirectAttributes.addFlashAttribute("Sucesso", "Produto cadastrado com sucesso!");
+		return new ModelAndView("redirect:produtos");
 	}
 	
 	@RequestMapping(method = RequestMethod.GET)
